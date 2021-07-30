be66f495b01ae9e83e2c54e45f98a9ae36696de6
Marcos Capistrano <mcapistrano50@gmail.com>
2021-07-30 15:51:48 -0300

    iasdsaq

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3308;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at ${PORT}`);
+  console.log("oi");
+});
