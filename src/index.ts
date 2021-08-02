4668cadf4c330512a9ceb1868c7badfe4c9a3199
Marcos Capistrano <mcapistrano50@gmail.com>
2021-08-02 14:21:07 -0300

    fix(dsf): sdfs

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ app.get("/", (req: express.Request, res: express.Response) => {
 app.listen(PORT, () => {
   console.log(`Example app listening at ${PORT}`);
   console.log("")
+  console.log("")
 
 });
 
